Fixed sample with bfloat16 precision

models/gpt2/sample.py
@@ -30,7 +30,15 @@ def sample_sequence(*, params, length, start_token=None, batch_size=None, contex
         context = tf.fill([batch_size, 1], start_token)
 
     def step(params, tokens, past=None):
-        lm_output = gpt2.model(params=params, X=tokens, past=past, reuse=tf.AUTO_REUSE)
+        if params["precision"] == 'bfloat16':
+            with tf.contrib.tpu.bfloat16_scope():
+                lm_output = gpt2.model(params=params, X=tokens, past=past, reuse=tf.AUTO_REUSE)
+
+            lm_output["logits"] = tf.cast(lm_output["logits"], tf.float32)
+            
+        else:
+            lm_output = lm_output = gpt2.model(params=params, X=tokens, past=past, reuse=tf.AUTO_REUSE)
+        
 
         logits = lm_output['logits'][:, :, :params["n_vocab"]]
         presents = lm_output['present']